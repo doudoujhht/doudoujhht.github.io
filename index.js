5c2833fa72a57ee023ffd89aab4a63dda0d0a647
doudoujhht <100162576+doudoujhht@users.noreply.github.com>
2022-06-07 21:16:00 -0400

    parallax effect website

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const thumb1 = document.getElementById('thumb1');
-const thumb2 = document.getElementById('thumb2');
-const thumb3 = document.getElementById('thumb3');
-const circle = document.getElementById('circle');
-const nom = document.getElementById('nom');
-const button = document.getElementById('button');
-const logo = document.getElementById('logo');
+let moon = document.getElementById("moon");
+let stars = document.getElementById("stars");
+let mountains_behind = document.getElementById("mountains_behind");
+let mountains_front = document.getElementById("mountains_front");
+let text = document.getElementById("text");
+let btn = document.getElementById("btn");
+let header = document.querySelector("header");
 
-function slide(number, color){
-    document.getElementById('main-image').src = `images/img${number}.png`;
-    circle.style.backgroundColor = color;
-    button.style.backgroundColor = color;
-    nom.style.color = color;
-    logo.src =`images/logo${number}.png`
-}
+window.addEventListener('scroll', ()=>{
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountains_behind.style.top = value * 0.5 + 'px'; 
+    mountains_front.style.top = value * 0 + 'px'; 
+    text.style.marginRight = value * 4 + 'px'; 
+    text.style.marginTop = value * 1.5 + 'px'; 
+    btn.style.marginTop = value * 1.5 + 'px'; 
+    header.style.top = value * 0.5 + 'px';
+})
